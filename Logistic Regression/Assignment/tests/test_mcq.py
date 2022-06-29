d1e5bb560ac8bece1430332e38f5b433268420b4
Lunde Chen <lunde.chen@yahoo.fr>
2022-06-29 16:01:32 +0800

    logistic regression

---
+++ Logistic Regression/Assignment/tests/test_mcq.py
@@ -0,0 +1,9 @@
+from nbresult import ChallengeResultTestCase
+
+
+class TestMcq(ChallengeResultTestCase):
+    def test_mcq(self):
+        student_ans = sorted(self.result.mcq.upper())
+        correct_ans = 'C'
+        self.assertEqual(student_ans, correct_ans)
+
