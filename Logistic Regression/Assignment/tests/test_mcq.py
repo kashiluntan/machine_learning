a8d630a4f19717ba1dda79327f802b59e1ab7a26
Lunde Chen <lunde.chen@yahoo.fr>
2022-06-30 12:44:42 +0800

    assignment week 3

--- Logistic Regression/Assignment/tests/test_mcq.py
+++ Logistic Regression/Assignment/tests/test_mcq.py
@@ -4,6 +4,6 @@ from nbresult import ChallengeResultTestCase
 class TestMcq(ChallengeResultTestCase):
     def test_mcq(self):
         student_ans = sorted(self.result.mcq.upper())
-        correct_ans = 'C'
+        correct_ans = ['C']
         self.assertEqual(student_ans, correct_ans)
 
